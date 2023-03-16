6a89a2549a3fa5450fcf47265ba9ccf1effc8a32
wartman <faxmaster@gmail.com>
2023-03-16 10:24:18 -0500

    Make status public

--- src/kit/http/HttpException.hx
+++ src/kit/http/HttpException.hx
@@ -3,7 +3,7 @@ package kit.http;
 import haxe.Exception;
 
 class HttpException extends Exception {
-	final status:StatusCode;
+	public final status:StatusCode;
 
 	public function new(status, message) {
 		super(message);
